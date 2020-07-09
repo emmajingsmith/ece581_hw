450a59428681621cc064543b51fa468958a8418c
emmajingsmith <43729294+emmajingsmith@users.noreply.github.com>
2020-07-09 09:21:51 -0700

    p1 source

---
+++ hw_2/p1.sv
@@ -0,0 +1,20 @@
+/*
+ * Name: Emma Smith
+ * Date: 22 July 2020
+ * Description: Small testbench to prove the initilization values of a
+ *               variable and logic net. 
+ *
+*/
+
+module p1;
+
+  var logic a;
+  logic b;
+
+  # 50
+
+  a = 0;
+  b = 0;
+
+  #10 $finish;
+endmodule;
